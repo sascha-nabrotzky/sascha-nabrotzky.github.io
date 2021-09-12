cf385741bc5ae5c46723900481cabf95d52743a9
Sascha <68822358+sascha-nabrotzky@users.noreply.github.com>
2021-09-12 19:14:11 +0200

    Add/Corrected forEach-loop for text change in hero module

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,23 +1,24 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import * as heroStyles from "./hero.module.scss"
 
 export default function Hero() {
-  const textArray = ["Frontend-", "Web-", "App-"]
-  let textItem
+  const textArray = ["Frontend-", "Web-", "PWA-", "Design-"]
+  let [state, setState] = useState(textArray[0])
 
-  setInterval(() => {
-    textArray.forEach((item, i) => {
-      setTimeout(() => {
-        textItem = item
-        console.log(textItem)
-      }, i * 1500)
-    })
-  }, 6000)
+  useEffect(() => {
+    setInterval(() => {
+      textArray.forEach((item, i) => {
+        setTimeout(() => {
+          setState(item)
+        }, i * 1500)
+      })
+    }, 6000)
+  }, [])
 
   return (
     <section className={heroStyles.herocontainer}>
       <div className={heroStyles.overflowcontainer}>
-        <p>{textArray[0]}</p>
+        <p dangerouslySetInnerHTML={{ __html: state }}></p>
       </div>
       <div className={heroStyles.overflowcontainer}>
         <p>Entwicklung</p>
