f2631e4b3f935026f70ecf986b7cd2ba97d5836c
Sascha <68822358+sascha-nabrotzky@users.noreply.github.com>
2021-09-23 20:43:47 +0200

    Correct text

--- src/pages/about.js
+++ src/pages/about.js
@@ -32,8 +32,8 @@ export default function About() {
           paar Preise habe ich gewonnen, doch für meine größte Passion legte ich
           mich noch weiter ins Zeug - ich lernte <strong>HTML5, SCSS</strong>{" "}
           und vor allem
-          <strong>JavaScript,</strong> gefolgt von <strong>React.js</strong> und{" "}
-          <strong>Gatsby.js,</strong> Projekte wurden von mir umgesetzt und
+          <strong> JavaScript,</strong> gefolgt von <strong>React.js</strong>{" "}
+          und <strong>Gatsby.js,</strong> Projekte wurden von mir umgesetzt und
           optimiert - damit stieg ich schließlich vollständig in die
           Frontend-Entwicklung ein. Mit jeder neuen Technologie eröffnen sich
           mir unglaublich spannende Möglichkeiten, die mich herausfordern und
