329bc1ac9f53c7faaa21b42444312de683fa64ab
Sascha <68822358+sascha-nabrotzky@users.noreply.github.com>
2021-12-18 12:26:40 +0100

    [BUGFIX] Set to https

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ export default function About() {
       <div>
         <h1>I can't find your Page!</h1>
         <img
-          src="http://25.media.tumblr.com/bb9a7e0e0cdc14dd816115c15b370975/tumblr_n036ieyQXV1qd5mq1o1_400.gif"
+          src="https://25.media.tumblr.com/bb9a7e0e0cdc14dd816115c15b370975/tumblr_n036ieyQXV1qd5mq1o1_400.gif"
           alt="Looking for your Page"
         ></img>
         <p>This Page and Website is still under construction!</p>
