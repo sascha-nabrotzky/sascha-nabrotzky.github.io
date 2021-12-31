e1db57990d75ba90f7350f6fcaacea24126dbd7b
Sascha Nabrotzky <sascha.nabrotzky@online.de>
2021-12-31 14:50:50 +0100

    correct useEffect hook

--- src/components/githubprojects.js
+++ src/components/githubprojects.js
@@ -4,12 +4,15 @@ import * as gitProjectStyles from "../components/githubprojects.module.scss"
 export default function FetchGithubProjects(props) {
   let [data, setData] = useState(null) //data auf null setzen, danach mit Funkt. setData die commits fetchen und Projektdaten mappen
 
-  useEffect(async () => {
-    let responseFromUrl = await fetch(
-      "https://api.github.com/users/sascha-nabrotzky/repos"
-    )
-    let commits = await responseFromUrl.json()
-    setData(commits)
+  useEffect(() => {
+    async function fetchMyAPI() {
+      let responseFromUrl = await fetch(
+        "https://api.github.com/users/sascha-nabrotzky/repos"
+      )
+      let commits = await responseFromUrl.json()
+      setData(commits)
+    }
+    fetchMyAPI()
   }, [])
 
   if (!data) return <p>Loading ...</p>
@@ -24,17 +27,18 @@ export default function FetchGithubProjects(props) {
               href={project.clone_url}
               rel="noreferrer noopener"
               target="_blank"
+              key={project.id}
             >
-              <div className={gitProjectStyles.projectsBox} key={project.id}>
+              <div className={gitProjectStyles.projectsBox}>
                 <h4 dangerouslySetInnerHTML={{ __html: project.name }}></h4>
                 <p
                   dangerouslySetInnerHTML={{ __html: project.description }}
                 ></p>
-                <div>
+                <div className={gitProjectStyles.projectTime}>
                   <p>Updated at:</p>
-                  <date
+                  <time
                     dangerouslySetInnerHTML={{ __html: project.updated_at }}
-                  ></date>
+                  ></time>
                 </div>
               </div>
             </a>
