4d29056498b0b9443989d9ace39b5ed4de3aa18d
Sascha Nabrotzky <sascha.nabrotzky@online.de>
2022-04-03 13:12:16 +0200

    Add Tiny Slider

--- src/components/githubprojects.jsx
+++ src/components/githubprojects.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react"
+import { tns } from "../../node_modules/tiny-slider/src/tiny-slider"
+import "../styling/tiny-slider.scss"
 import * as gitProjectStyles from "../styling/githubprojects.module.scss"
 
 export default function FetchGithubProjects(props) {
@@ -18,6 +20,28 @@ export default function FetchGithubProjects(props) {
       setData(commits)
     }
     fetchMyAPI()
+
+    setTimeout(() => {
+      var slider = tns({
+        container: ".projectSlider",
+        items: 1,
+        slideBy: "page",
+        nav: false,
+        autoplay: true,
+        autoplayButtonOutput: false,
+        controlsPosition: "bottom",
+        gutter: 20,
+        controlsText: ["&lang;", "&rang;"],
+        responsive: {
+          640: {
+            items: 2,
+          },
+          900: {
+            items: 3,
+          },
+        },
+      })
+    }, 500)
   }, [])
 
   if (!data) return <p>Loading ...</p>
@@ -25,28 +49,31 @@ export default function FetchGithubProjects(props) {
   return (
     <section>
       <h2>Projekte auf Github</h2>
-      <div className={gitProjectStyles.projectsWrapper}>
+
+      <div class="projectSlider">
         {data.map(project => {
-          let [day, time] = project.updated_at.split("T")
+          let [day] = project.updated_at.split("T")
 
           return (
-            <a
-              href={project.clone_url}
-              rel="noreferrer noopener"
-              target="_blank"
-              key={project.id}
-            >
-              <div className={gitProjectStyles.projectsBox}>
-                <h4 dangerouslySetInnerHTML={{ __html: project.name }}></h4>
-                <p
-                  dangerouslySetInnerHTML={{ __html: project.description }}
-                ></p>
-                <div className={gitProjectStyles.projectTime}>
-                  <span>Updated at: </span>
-                  <time>{day}</time>
+            <div className={gitProjectStyles.boxWrapper}>
+              <a
+                href={project.clone_url}
+                rel="noreferrer noopener"
+                target="_blank"
+                key={project.id}
+              >
+                <div className={gitProjectStyles.projectsBox}>
+                  <h4 dangerouslySetInnerHTML={{ __html: project.name }}></h4>
+                  <p
+                    dangerouslySetInnerHTML={{ __html: project.description }}
+                  ></p>
+                  <div className={gitProjectStyles.projectTime}>
+                    <span>Updated at: </span>
+                    <time>{day}</time>
+                  </div>
                 </div>
-              </div>
-            </a>
+              </a>
+            </div>
           )
         })}
       </div>
