304be89271e3464f1be017d5c5106c365ec19857
Sascha Nabrotzky <sascha.nabrotzky@online.de>
2022-08-07 10:48:40 +0200

    Add optimized skills area and projects

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,14 +2,21 @@ import React, { useState, useEffect } from "react"
 import * as heroStyles from "../styling/hero.module.scss"
 
 export default function Hero() {
-  const textArray = ["TYPO3-", "CSS3-Design-", "React/Vue-"]
-  let [state, setState] = useState(textArray[0])
+  const textFirstLine = ["TYPO3 CMS", "CSS3-Design-", "Responsive"]
+  const textSecondLine = ["React/Vue", "System", "Websites"]
+  let [state, setState] = useState(textFirstLine[0])
+  let [state2, setState2] = useState(textSecondLine[0])
 
   useEffect(() => {
     setInterval(() => {
-      textArray.forEach((item, i) => {
+      textFirstLine.forEach((item, i) => {
         setTimeout(() => {
           setState(item)
+        }, i * 1500)
+      })
+      textSecondLine.forEach((item, i) => {
+        setTimeout(() => {
+          setState2(item)
         }, i * 1500)
       })
     }, 4500)
@@ -21,7 +28,7 @@ export default function Hero() {
         <p dangerouslySetInnerHTML={{ __html: state }}></p>
       </div>
       <div className={heroStyles.overflowcontainer}>
-        <p>Entwicklung</p>
+        <p dangerouslySetInnerHTML={{ __html: state2 }}></p>
       </div>
     </section>
   )
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -18,6 +18,7 @@ import logoCSS3 from "../images/CSS3_Logo-min.svg"
 import logoBootstrap from "../images/bootstrap-logo-min.svg"
 import logoNpm from "../images/npm-logo-min.svg"
 import logoDDEV from "../images/DDEV-Logo_min.svg"
+import logoTailwind from "../images/TailwindCSS_Logo.svg"
 import socMedImg from "../images/socMedImg.jpg"
 
 export default function Home() {
@@ -101,81 +102,92 @@ export default function Home() {
         </p>
         <LineSvg />
         <aside className={indexStyle.techStackWrapper}>
-          <h2>Meine Skills</h2>
-          <div className={indexStyle.logoWrapper}>
-            <img
-              src={logoTypo3}
-              title="TYPO3 CMS"
-              alt="TYPO3-Logo"
-              loading="lazy"
-            ></img>
-            <img
-              src={logoHTML5}
-              title="HTML5"
-              alt="HTML5-Logo"
-              loading="lazy"
-            ></img>
-            <img
-              src={logoCSS3}
-              title="CSS3"
-              alt="CSS3-Logo"
-              loading="lazy"
-            ></img>
-            <img
-              src={logoSass}
-              title="SASS/SCSS Preprozessor"
-              alt="SASS-Logo"
-              loading="lazy"
-            ></img>
-            <img
-              src={logoJs}
-              title="JavaScript"
-              alt="JS-Logo"
-              loading="lazy"
-            ></img>
-            <img
-              src={logoReact}
-              title="React"
-              alt="React-Logo"
-              loading="lazy"
-            ></img>
-            <img src={logoVue} title="Vue" alt="Vue-Logo" loading="lazy"></img>
-            <img
-              src={logoGatsby}
-              title="Gatsby - A Static Site Generator"
-              alt="Gatsby-Logo"
-              loading="lazy"
-            ></img>
-            <img
-              src={logoBootstrap}
-              title="Bootstrap 5 Framework"
-              alt="Bootstrap-Logo"
-              loading="lazy"
-            ></img>
-            <img
-              src={logoGit}
-              title="Git - Versionierung"
-              alt="Git-Logo"
-              loading="lazy"
-            ></img>
-            <img
-              src={logoNpm}
-              title="npm - node package manager"
-              alt="Npm-Logo"
-              loading="lazy"
-            ></img>
-            <img
-              src={logoDDEV}
-              title="DDEV"
-              alt="DDEV-Logo"
-              loading="lazy"
-            ></img>
-            <img
-              src={logoDocker}
-              title="Docker"
-              alt="Docker-Logo"
-              loading="lazy"
-            ></img>
+          <div>
+            <h2>Hauptsächlich nutze ich</h2>
+            <div className={indexStyle.logoWrapper}>
+              <img
+                src={logoTypo3}
+                title="TYPO3 CMS"
+                alt="TYPO3-Logo"
+                loading="lazy"
+              ></img>
+              <img
+                src={logoBootstrap}
+                title="Bootstrap 5 Framework"
+                alt="Bootstrap-Logo"
+                loading="lazy"
+              ></img>
+              <img
+                src={logoHTML5}
+                title="HTML5"
+                alt="HTML5-Logo"
+                loading="lazy"
+              ></img>
+              <img
+                src={logoCSS3}
+                title="CSS3"
+                alt="CSS3-Logo"
+                loading="lazy"
+              ></img>
+              <img
+                src={logoSass}
+                title="SASS/SCSS Preprozessor"
+                alt="SASS-Logo"
+                loading="lazy"
+              ></img>
+              <img
+                src={logoGit}
+                title="Git - Versionierung"
+                alt="Git-Logo"
+                loading="lazy"
+              ></img>
+              <img
+                src={logoDDEV}
+                title="DDEV"
+                alt="DDEV-Logo"
+                loading="lazy"
+              ></img>
+            </div>
+
+            <h2>Für meine eigenen Projekte zusätzlich</h2>
+            <div className={indexStyle.logoWrapper}>
+              <img
+                src={logoJs}
+                title="JavaScript"
+                alt="JS-Logo"
+                loading="lazy"
+              ></img>
+              <img
+                src={logoReact}
+                title="React"
+                alt="React-Logo"
+                loading="lazy"
+              ></img>
+              <img
+                src={logoGatsby}
+                title="Gatsby - A Static Site Generator"
+                alt="Gatsby-Logo"
+                loading="lazy"
+              ></img>
+              <img
+                src={logoVue}
+                title="Vue"
+                alt="Vue-Logo"
+                loading="lazy"
+              ></img>
+              <img
+                src={logoTailwind}
+                title="Tailwind-CSS"
+                alt="Tailwind-CSS"
+                loading="lazy"
+              ></img>
+              <img
+                src={logoNpm}
+                title="npm - node package manager"
+                alt="Npm-Logo"
+                loading="lazy"
+              ></img>
+            </div>
           </div>
         </aside>
         <GithubProjects />
