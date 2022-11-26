9b102ca5e96cd3285924b68ef0e9d573fddea76d
Sascha Nabrotzky <sascha.nabrotzky@online.de>
2022-11-26 13:16:35 +0100

    Add automaticly project elements to menu

--- src/components/sidemenu.jsx
+++ src/components/sidemenu.jsx
@@ -6,47 +6,22 @@ export default function Sidemenu() {
 
   useEffect(() => {
     const headlines = document.querySelectorAll("h3")
-    setListItem(Array.from(headlines))
+    const headlinesArray = Array.from(headlines)
+
+    const liElements = headlinesArray.map(item => {
+      // Map awaits a return
+      return (
+        <li className={style.menuitem}>
+          <a className={style.menulink} href={`#${item.id}`}>
+            {" "}
+            {item.innerText}
+          </a>
+        </li>
+      )
+    })
+
+    setListItem(liElements)
   }, [setListItem])
 
-  return (
-    <ul className={style.sidemenu}>
-      <li className={style.menuitem}>
-        <a className={style.menulink} href={`#${listItem[0].id}`}>
-          {" "}
-          {listItem[0].innerText}
-        </a>
-      </li>
-      <li className={style.menuitem}>
-        <a className={style.menulink} href={`#${listItem[1].id}`}>
-          {" "}
-          {listItem[1].innerText}
-        </a>
-      </li>
-      <li className={style.menuitem}>
-        <a className={style.menulink} href={`#${listItem[2].id}`}>
-          {" "}
-          {listItem[2].innerText}
-        </a>
-      </li>
-      <li className={style.menuitem}>
-        <a className={style.menulink} href={`#${listItem[3].id}`}>
-          {" "}
-          {listItem[3].innerText}
-        </a>
-      </li>
-      <li className={style.menuitem}>
-        <a className={style.menulink} href={`#${listItem[4].id}`}>
-          {" "}
-          {listItem[4].innerText}
-        </a>
-      </li>
-      <li className={style.menuitem}>
-        <a className={style.menulink} href={`#${listItem[5].id}`}>
-          {" "}
-          {listItem[5].innerText}
-        </a>
-      </li>
-    </ul>
-  )
+  return <ul className={style.sidemenu}>{listItem}</ul>
 }
