9c7aa016df76b8095c46f60c87ff99841170a6f9
Sascha Nabrotzky <sascha.nabrotzky@online.de>
2023-06-23 08:12:45 +0200

    add small hero with route condition

---
+++ src/components/hero_small.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import * as style from "../styles/hero_small.module.scss"
+
+export default function Hero() {
+
+  return (
+    <section className={style.herocontainer}>
+      <h3 className={style.heroSmallHeading}> HERO SMALL </h3>
+    </section>
+  )
+}
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,17 +1,28 @@
 import React from "react"
+import { Location } from '@reach/router'
 import Footer from "./footer"
 import Header from "./header"
 import Hero from "./hero"
+import HeroSmall from "./hero_small"
 import ToTopButton from "./ToTopButton"
 import Snowflakes from "../components/snowflakes"
 import * as layoutStyles from "../styles/layout.module.scss"
 
 export default function Layout({ children }) {
+
   return (
     <div className={layoutStyles.container}>
       <Snowflakes />
       <Header />
-      <Hero />
+
+      <Location>
+        {({ location }) => {
+          if (location.pathname === '/') return <Hero />
+          return <HeroSmall />
+        }}
+      </Location>
+
+      
       <main className={layoutStyles.content}>{children}</main>
       <ToTopButton />
       <Footer />
