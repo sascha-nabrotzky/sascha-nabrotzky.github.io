61c005cb6241978b4639ee7ffdc09ef9ef5aa6c5
Sascha Nabrotzky <sascha.nabrotzky@online.de>
2023-10-31 09:29:00 +0100

    optimize text and code

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,16 +2,13 @@ import React from "react"
 import * as styles from "../styles/hero.module.css"
 
 function Hero() {
-  const textFirstLine = ["User-Experience", "Frontend-", "Grafik /"]
-  const textSecondLine = ["Engineer", "Entwicklung", "Illustration"]
-
   return (
     <section className={styles.herocontainer}>
       <div className={styles.overflowcontainer}>
-        <p>{textFirstLine[0]}</p>
+        <p>Webdesign /</p>
       </div>
       <div className={styles.overflowcontainer}>
-        <p>{textSecondLine[0]}</p>
+        <p>UX-Design</p>
       </div>
     </section>
   )
